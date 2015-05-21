7e83c270c3bb911dd7392d6ea6a81fdecfa0104e
Kazuyuki TAKASE <tkskzyk0925@gmail.com>
2015-05-22 02:00:37 +0900

    コマンドラインパーサを実装

--- main.py
+++ main.py
@@ -3,28 +3,70 @@
 """
 モーションインストール用CUIアプリケーション
 """
-__author__ = "Kazuyuki TAKASE <takase@plen.jp>"
-__status__ = "2.0.0"
-__date__   = "2015/05/19"
+__author__  = "Kazuyuki TAKASE"
+__license__ = "MIT"
+__status__  = "2.0.0"
+__date__    = "2015/05/22"
+__email__   = "takase@plen.jp"
 
 
-from optparse import OptionParser
+# 標準ライブラリのインポート
+import sys
+from argparse import ArgumentParser
 
+# 自作ライブラリのインポート
+sys.path.append("./lib/")
+import BLED112_Driver
+
+
+# メインスレッド
+def main(args):
+	send_device = None
+	if args.send == "bled112":
+		send_device = BLED112_Driver()
+	if args.send == "internal_ble":
+		# send_device = InternalDriver()
+
+	if (send_device == None):
+		exit()
+
+
+# アプリケーション・エントリーポイント
 if __name__ == "__main__":
 	# コマンドラインオプションの定義
-	option_parser = OptionParser()
-	option_parser.add_option(
-		"-c", "--com",
-
+	argument_parser = ArgumentParser()
+	argument_parser.add_argument(
+		"-s", "--send",
+		dest    = "send",
+		choices = ("bled112", "internal_ble"),
+		default = "bled112",
+		help    = u'送信デバイスを"bled112", "internal_ble"から選択します。',
+		metavar = "<SEND DEVICE>"
 	)
-	option_parser.add_option(
+	argument_parser.add_argument(
+		"-r", "--receive",
+		dest    = "receive",
+		help    = u"受信デバイスを指定します。",
+		metavar = "<RECEIVE DEVICE>" 
+	)
+	argument_parser.add_argument(
 		"-f", "--file",
-		dest    = "motions",
-		help    = "MOTIONS に指定したモーションファイルをインストールします。",
-		metavar = "MOTIONS"
+		dest     = "motions",
+		nargs    = "+",
+		required = True,
+		help     = u"指定したモーションファイルをインストールします。",
+		metavar  = "<MOTION>"
 	)
-	option_parser.add_option(
+	argument_parser.add_argument(
 		"-v", "--verify",
-
+		dest    = "verify_flag",
+		action  = "store_true",
+		default = False,
+		help    = u"モーションファイルのインストール後、書き込みの検証を行います。"
 	)
 
+	# コマンドラインオプションの検証，および取得
+	args = argument_parser.parse_args()
+
+	# メインスレッドの起動
+	main(args)
